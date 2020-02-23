c4a7ca56b03634ecff3a2b6b3697d4d1c474ab79
Girish Hari <bunnefant@gmail.com>
2020-02-23 07:35:05 -0500

    added url to html parser

--- src/main/java/com/baitbully/middleware/CheckBait.java
+++ src/main/java/com/baitbully/middleware/CheckBait.java
@@ -95,11 +95,43 @@ public class CheckBait implements RequestStreamHandler {
 		return results;
 	}
 	
-	public ArrayList<String> getTitles(String url) {
-	   
-	   
-		return null;
-	}
+	public static ArrayList<String> getTitles(String url) throws Exception {
+      String html = getHTML(url);
+      ArrayList<String> bruh = new ArrayList<>();
+      int begin = 0;
+      int stringStart;
+      String key = "BNeawe vvjwJb AP7Wnd";
+      String title = "";
+      for (int i = 0; i < 10; i++)   {
+         stringStart = html.indexOf(key, begin) + 2 + key.length();
+         title = html.substring(stringStart, html.indexOf("<", stringStart));
+         while(title.contains("&#")) {
+            title = title.replaceAll(title.substring(title.indexOf("&#"), title.indexOf("&#") + 7 ), "'");
+         }
+         bruh.add(title);
+         begin = stringStart;
+      }
+      return bruh;
+   }
+   
+   public static String getHTML(String urlToRead) throws Exception {
+      StringBuilder result = new StringBuilder();
+      URL url = new URL(urlToRead);
+      
+      HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+      conn.addRequestProperty("User-Agent", "Mozilla");
+      conn.setReadTimeout(500000);
+      conn.setConnectTimeout(500000);
+
+      conn.setRequestMethod("GET");
+      BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+      String line;
+      while ((line = rd.readLine()) != null) {
+         result.append(line);
+      }
+      rd.close();
+      return result.toString();
+   }
    
    
 }
